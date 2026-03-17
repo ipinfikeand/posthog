8d9029721060d10a749acc774cb4b87d5db58659
Lucas Ricoy <lucas.r@posthog.com>
2026-03-17 11:14:02 -0300

    feat: remove cookieless_server_hash_mode team setting

--- frontend/src/scenes/settings/types.ts
+++ frontend/src/scenes/settings/types.ts
@@ -195,7 +195,6 @@ export type SettingId =
     | 'web-vitals-autocapture'
     | 'dead-clicks-autocapture'
     | 'channel-type'
-    | 'cookieless-server-hash-mode'
     | 'user-groups'
     | 'user-delete'
     | 'web-revenue-events'
