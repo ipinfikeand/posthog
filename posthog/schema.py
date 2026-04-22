5bb5f28e7bca39b32804a32978992c52cc4039c4
Tomás Farías Santana <tomas@tomasfarias.dev>
2026-04-22 17:04:22 +0200

    chore: Update schema.py

--- posthog/schema.py
+++ posthog/schema.py
@@ -2679,6 +2679,7 @@ class IntegrationKind(StrEnum):
     CUSTOMERIO_APP = "customerio-app"
     CUSTOMERIO_WEBHOOK = "customerio-webhook"
     CUSTOMERIO_TRACK = "customerio-track"
+    POSTGRESQL = "postgresql"
 
 
 class IntervalType(StrEnum):
